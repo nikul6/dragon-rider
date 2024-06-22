455bdfab0b5149fbc9abc5e39b9b1deb41dd874d
nikul6 <nikuldhameliya6@gmail.com>
2024-06-22 18:44:46 +0530

    add custom icon as user location

--- App.tsx
+++ App.tsx
@@ -3,7 +3,9 @@ import houses from './data/houses.json';
 import { featureCollection, point } from '@turf/helpers'
 import { getDirections } from './services/directions';
 import { OnPressEvent } from '@rnmapbox/maps/lib/typescript/src/types/OnPressEvent';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
+import pin from './assets/pin.png';
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
 
@@ -12,6 +14,7 @@ export default function App() {
   const [direction, setDirection] = useState([])
   const points = houses.map((house) => point([house.longitude, house.latitude], { image: house.image }));
   const directionCoordinate = direction?.routes?.[0]?.geometry.coordinates;
+  const [userLocation, setUserLocation] = useState(null);
 
   const imageUrls = houses.reduce((acc, house) => {
     acc[house.image] = { uri: house.image };
@@ -23,10 +26,35 @@ export default function App() {
     setDirection(newDirection)
   }
 
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setUserLocation([location.coords.longitude, location.coords.latitude]);
+    })();
+  }, []);
+
   return (
     <MapView style={{ flex: 1 }} styleURL='mapbox://styles/mapbox/streets-v12'>
       <Camera zoomLevel={10} followUserLocation pitch={60} animationDuration={6000} />
-      <LocationPuck puckBearingEnabled puckBearing='course' pulsing={{ isEnabled: true }} />
+      {userLocation && (
+        <ShapeSource id='userLocation' shape={featureCollection([point(userLocation)])}>
+          <SymbolLayer
+            id='userLocationIcon'
+            style={{
+              iconImage: 'customUserLocationIcon',
+              iconSize: 0.8,
+            }}
+          />
+          <Images images={{ customUserLocationIcon: pin }} />
+        </ShapeSource>
+      )}
+      {/* <LocationPuck puckBearingEnabled puckBearing='course' pulsing={{ isEnabled: true }} /> */}
       <ShapeSource id='scooters' cluster shape={featureCollection(points)} onPress={onPointPress}>
         <SymbolLayer
           id='clusters-point'
